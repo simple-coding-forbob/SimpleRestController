90a8d9f7e9c91cc4822e62e5588183dfde6ff0a1
forbob <forbob@naver.com>
2026-01-18 09:34:30 +0900

    1
    1

--- SimpleRestController/src/main/java/com/simplecoding/simplerestcontroller/controller/ex02/ParamController.java
+++ SimpleRestController/src/main/java/com/simplecoding/simplerestcontroller/controller/ex02/ParamController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ParamController {
-	
+    
 //	따라하기 예제 1
     @GetMapping("/ex02/example01")
     public ApiResponse<String> example01(@RequestParam(defaultValue = "") String name) {
